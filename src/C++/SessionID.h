a554c6f319c718010b0cde0fc907d9ccc148a32b
Jason Aubrey <jason@RedBull.local>
2009-07-20 14:21:38 -0500

    Fixed issue with copywright info at the top of the file

--- src/C++/SessionID.h
+++ src/C++/SessionID.h
@@ -1,4 +1,7 @@
-opyright (c) quickfixengine.org  All rights reserved.
+/* -*- C++ -*- */
+
+/****************************************************************************
+** Copyright (c) quickfixengine.org  All rights reserved.
 **
 ** This file is part of the QuickFIX FIX Engine
 **
@@ -28,14 +31,6 @@ class SessionID
 {
 public:
   SessionID() {}
-
-  engine.org and appearing in the file
-** LICENSE included in the packaging of this file.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-** See http://www.quickfixengine.org/LICENSE for licensing informatialifier( sessionQualifier ) 
   {
         toString(m_frozenString);
   }
